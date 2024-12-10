4312444be9db485a1bbc86549919850b956f8215
Pranjal Rai <pranjalrai230634@acropolis.in>
2024-12-10 11:12:34 +0000

    day1 end

--- day1.py
+++ day1.py
@@ -1,16 +1,22 @@
 import numpy as np 
 
-arr=np.array([[1,2,3,4,5],
-              [6,7,8,9,10],
-              [11,12,13,14,15],
-              [16,17,18,19,20]])
-# print(arr[1: :2])
-# print(arr[::-1])
-# print(arr[2,3])
+# arr=np.array([[1,2,3,4,5],
+#               [6,7,8,9,10],
+#               [11,12,13,14,15],
+#               [16,17,18,19,20]])
+# # print(arr[1: :2])
+# # print(arr[::-1])
+# # print(arr[2,3])
 
 
-print(arr*5)
+# print(arr*5)
 
-print(arr+5)
+# print(arr+5)
 
-print(arr-5)
+# print(arr-5)
+
+arr3 = np.random.randint(10,50,125).reshape(5,5,5)
+print(arr3)
+print(arr3.ndim)
+print(arr3.shape)
+print(arr3)
