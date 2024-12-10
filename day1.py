98849dc56cbefa13e8e59d03b2315992196ce54d
Pranjal Rai <pranjalrai230634@acropolis.in>
2024-12-10 11:01:26 +0000

    new update

--- day1.py
+++ day1.py
@@ -4,8 +4,9 @@ arr=np.array([[1,2,3,4,5],
               [6,7,8,9,10],
               [11,12,13,14,15],
               [16,17,18,19,20]])
-print(arr[1: :2])
-print(arr[::-1])
-print(arr[2,3])
+# print(arr[1: :2])
+# print(arr[::-1])
+# print(arr[2,3])
 
-g
+
+print(arr*5)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print(arr[1: :2])
+# print(arr[::-1])
+# print(arr[2,3])
